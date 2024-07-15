01ad392b3b4acf41a915be6cd7376fdcd9072439
Indika senarathna <indikai002i@gmail.com>
2024-07-15 21:14:09 +0530

    build 06 of this

--- helloworld.java
+++ helloworld.java
@@ -4,8 +4,8 @@ public class helloworld {
         System.out.println("hello jenkins..........");
 
         for (int i=0;i<10;i++){
-            int k=i*2;
-            System.out.println("No: "+k);
+            int k=i2;
+            System.out.prinln("No: "+k);
         }
         for(int j=0;j<20;j+){
             System.out.println("new No: "+j);
