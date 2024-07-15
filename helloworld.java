5363dc2c7cf8a897e38a8411902ac61d2d223730
Indika senarathna <indikai002i@gmail.com>
2024-07-15 20:17:45 +0530

    new loop 20

--- helloworld.java
+++ helloworld.java
@@ -3,8 +3,11 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello jenkins..........");
 
-        for (int i=0;i<10;i++){
-            System.out.println("No: "+i);
+//        for (int i=0;i<10;i++){
+//            System.out.println("No: "+i);
+//        }
+        for(int j=0;j<20;j++){
+            System.out.println("new No: "+j);
         }
     }
 }
