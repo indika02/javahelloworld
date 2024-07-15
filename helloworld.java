fa93c9e88e16fb1c62694d03161ebf066ef40e40
Indika senarathna <indikai002i@gmail.com>
2024-07-15 19:58:32 +0530

    helloworldbuild01

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("hello jenkins..........");
+
+        for (int i=0;i<10;i++){
+            System.out.println("No: "+i);
+        }
+    }
+}
