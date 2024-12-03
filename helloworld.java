3142b269048df8f43562180382f557918488be00
indika02 <indikai002i@gmail.com>
2024-12-03 23:11:16 +0530

    error

--- helloworld.java
+++ helloworld.java
@@ -5,7 +5,8 @@ public class helloworld {
 
         for (int i=0;i<10;i++){
             int k=i*2;
-            System.out.println("No: "+k);
+            System.out.println("No: ",k);
+
         }
 
     }
